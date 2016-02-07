52ad421eb16b5c29ad00ceed0b862846bf167e63
Uranuz <neuranuz@gmail.com>
2016-02-07 12:17:51 +0300

    Добавил кэш шаблонов

--- src/webtank/templating/plain_templater.d
+++ src/webtank/templating/plain_templater.d
@@ -25,10 +25,9 @@ class Element
 {	immutable(size_t) prePos;
 	immutable(size_t) sufPos;
 	immutable(size_t) matchOpPos;
-	dstring subst;
 	this( size_t prefixPos, size_t suffixPos, size_t matchOperatorPos = size_t.max )
 	{	prePos = prefixPos; sufPos = suffixPos; matchOpPos = matchOperatorPos; }
-	bool isVar() @property
+	bool isVar() @property const
 	{	return matchOpPos != size_t.max;
 	}
 }
@@ -48,9 +47,9 @@ static this()
 	} //with( LexemeType )
 }
 
-class PlainTemplater
-{	
-protected:
+class PlainTemplate
+{
+//private;
 	Element[][dstring] _namedEls;
 	Element[] _indexedEls;
 	dstring _sourceStr;
@@ -63,58 +62,12 @@ public:
 		_sourceStr = std.utf.toUTF32(templateStr);
 		_parseTemplateStr();
 	}
-	
-	//Устанавливает замещающее значение value для метки с именем markName
-	void set(string markName, string value)
-	{	import std.utf;
-		dstring markNameUTF32 = std.utf.toUTF32(markName);
-		if( markNameUTF32 in _namedEls )
-		{	foreach(el; _namedEls[markNameUTF32])
-			{	el.subst = std.utf.toUTF32(value);
-			}
-		}
-		else
-		{	//TODO: Записать в лог ошибок, что данного элемента нету 
-			
-		}
-	}
-	
-	//Получение значения из переменной с именем name
-	string get(string name)
-	{	import std.utf;
-		dstring nameUTF32 = std.utf.toUTF32(name);
-		if( (nameUTF32 in _namedEls) && (_namedEls[nameUTF32].length > 0) )
-		{	auto el = _namedEls[nameUTF32][0];
-			if( el.isVar )
-				return 
-					std.utf.toUTF8( _sourceStr[ (el.matchOpPos + _lexValues[LexemeType.matchOp].length) .. el.sufPos ] );
-			else 
-				return null;
-		}
-		else 
-			return null;
-	}
-	
-	string getString()
-	{	import std.utf;
-		dstring result;
-		size_t textStart = 0;
-		foreach(el; _indexedEls)
-		{	if( el.isVar )
-			{	result ~= _sourceStr[textStart .. el.prePos];
-				textStart = el.sufPos + _lexValues[LexemeType.varSuf].length;
-			}
-			else
-			{	result ~= _sourceStr[textStart .. el.prePos] ~ el.subst;
-				textStart = el.sufPos + _lexValues[LexemeType.markSuf].length;
-			}
-		}
-		result ~= _sourceStr[textStart .. $];
-		return std.utf.toUTF8(result);
-	}
-protected:
+
+private:
 	void _parseTemplateStr()
 	{	
+		import std.algorithm: startsWith;
+		
 		size_t prefixPos;
 		size_t matchOpPos;
 		bool markPreFound = false;
@@ -124,34 +77,29 @@ protected:
 		for( size_t i = 0; i < _sourceStr.length; ++i )
 		{	dstring[LexemeType] selLexemes;
 			foreach( lexType, curLexValue; _lexValues )
-			{	if( (i + curLexValue.length) < _sourceStr.length )
-				{	if( _sourceStr[i .. (i + curLexValue.length) ] == curLexValue )
-					{	//Если нашли, то добавляем в сортированный список новый элемент
-						bool checked = true;
-						with( LexemeType )
-						{
-						switch( lexType )
-						{	
-							case matchOp: {
-								checked = varPreFound;
-								break;
-							}
-							case varSuf: {
-								checked = varPreFound && varMatchOpFound;
-								break;
-							}
-							case markSuf: {
-								checked = markPreFound;
-								break;
-							}
-							default:
-							
+			{	if( _sourceStr[i..$].startsWith(curLexValue) )
+				{
+					//Если нашли, то добавляем в сортированный список новый элемент
+					bool checked = true;
+					switch( lexType ) with( LexemeType )
+					{	
+						case matchOp: {
+							checked = varPreFound;
 							break;
 						}
-						} //with( LexemeType )
-						if( checked )
-							selLexemes[lexType] ~= curLexValue;
+						case varSuf: {
+							checked = varPreFound && varMatchOpFound;
+							break;
+						}
+						case markSuf: {
+							checked = markPreFound;
+							break;
+						}
+						default:
+							break;
 					}
+					if( checked )
+						selLexemes[lexType] ~= curLexValue;
 				}
 			}
 			
@@ -168,9 +116,7 @@ protected:
 				}
 			}
 			
-			with( LexemeType )
-			{
-			switch( selLexType )
+			switch( selLexType ) with( LexemeType )
 			{
 				case matchOp: {
 					varMatchOpFound = true;
@@ -214,16 +160,153 @@ protected:
 				
 				break;
 			}
-			} //with( LexemeType )
 		}
+	}
+	
+	size_t lexLen(LexemeType lexType) const
+	{
+		return _lexValues[lexType].length;
+	}
+	
+	public string getString(const dstring[dstring] values, size_t extraSize = 0) const
+	{	import std.utf;
+		import std.array: appender;
+		
+		auto result = appender!dstring();
+		result.reserve( _sourceStr.length + extraSize );
+		//dstring result;
+		size_t textStart = 0;
+		dstring markName;
+		foreach(el; _indexedEls)
+		{	
+			if( el.isVar )
+			{	result ~= _sourceStr[textStart .. el.prePos];
+				textStart = el.sufPos + lexLen(LexemeType.varSuf);
+			}
+			else
+			{	markName = std.string.strip( 
+					_sourceStr[ el.prePos + lexLen(LexemeType.markPre) .. el.sufPos ]
+				);
+				result ~= _sourceStr[textStart .. el.prePos] ~ values.get(markName, null);
+				textStart = el.sufPos + lexLen(LexemeType.markSuf);
+			}
+		}
+		result ~= _sourceStr[textStart .. $];
+		return std.utf.toUTF8( result.data() );
+		//return std.utf.toUTF8( result );
 	}
 }
 
-/*void main()
-{	import std.stdio;
-	auto tempter = new PlainTemplater(testTemplateStr);
-	tempter.substitude("Вася", "content");
-	writeln( tempter.getStr() );
-	writeln(tempter.getValue("page_title"));
+
+class PlainTemplater
+{	
+private:
+	PlainTemplate _tpl;
+	dstring[dstring] _values;
+	size_t _extraDataSizeEval; //Доп. объем данных которые могут быть записаны в вывод
 	
-}*/
+public:
+	this( string templateStr, immutable(dstring[LexemeType]) lexemes = defaultLexems )
+	{	
+		_tpl = new PlainTemplate(templateStr, lexemes);
+	}
+	
+	this(PlainTemplate tpl)
+	{
+		_tpl = tpl;
+	}
+	
+	//Устанавливает замещающее значение value для метки с именем markName
+	void set(string markName, string value)
+	{	import std.utf;
+		dstring markNameUTF32 = std.string.strip( std.utf.toUTF32(markName) );
+		dstring valueUTF32 = std.utf.toUTF32( value );
+		
+		//Собираем оценку хранимых данных
+		//Если по новой задаем переменную, то вычитаем предыдущий размер данных из оценки размера
+		size_t oldValueSize = _values.get(markNameUTF32, null).length;
+		
+		if( _extraDataSizeEval >= oldValueSize ) //Перестраховка, чтобы не было переполнения при вычитании (хотя не должно быть!)
+			_extraDataSizeEval -= oldValueSize;
+			
+		_extraDataSizeEval += valueUTF32.length;
+		
+		_values[markNameUTF32] = valueUTF32;
+	}
+	
+	//Получение значения из переменной с именем name
+	string get(string name) const
+	{	import std.utf;
+		dstring nameUTF32 = std.utf.toUTF32(name);
+		if( (nameUTF32 in _tpl._namedEls) && (_tpl._namedEls[nameUTF32].length > 0) )
+		{	auto el = _tpl._namedEls[nameUTF32][0];
+			if( el.isVar )
+				return 
+					std.utf.toUTF8( _tpl._sourceStr[ ( el.matchOpPos + _tpl.lexLen(LexemeType.matchOp) ) .. el.sufPos ] );
+			else 
+				return null;
+		}
+		else 
+			return null;
+	}
+	
+	string getString() const
+	{
+		return _tpl.getString(_values, _extraDataSizeEval);
+	}
+
+}
+
+class PlainTemplateCache(bool useCache = true)
+{
+private:
+	import core.sync.mutex;
+	PlainTemplate[string] _tpls;
+	
+	Mutex _mutex;
+	
+public:
+	this()
+	{
+		_mutex = new Mutex();
+	}
+	
+	PlainTemplater get(string fileName)
+	{
+		import std.file: read, exists, FileException;
+
+		static if( useCache )
+		{
+			PlainTemplate tpl;
+			
+			if( fileName in _tpls )
+			{
+				tpl = _tpls[fileName];
+			}
+			else
+			{
+				if( !exists(fileName) )
+					throw new FileException(fileName, "Template file '" ~ fileName ~ "' not found!");
+				
+				synchronized( _mutex )
+				{
+					string templateStr = cast(string) read( fileName );
+					tpl = new PlainTemplate(templateStr);
+					_tpls[fileName] = tpl;
+				}
+			}
+			
+			return new PlainTemplater( tpl );
+		}
+		else
+		{
+			if( !exists(fileName) )
+				throw new FileException(fileName, "Template file '" ~ fileName ~ "' not found!");
+			
+			string templateStr = cast(string) read( fileName );
+			return new PlainTemplater( templateStr );
+		}
+	}
+
+}
+
