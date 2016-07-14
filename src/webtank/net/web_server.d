bd0c3bce03eadd59f2e97bff264fc2e978b4b1be
Uranuz <neuranuz@gmail.com>
2016-07-14 22:43:13 +0300

    Написана альтернативная реализация веб-сервера на основе пула задач с фиксированным числом нитей исполнения

--- src/webtank/net/web_server.d
+++ src/webtank/net/web_server.d
@@ -4,6 +4,7 @@ import std.socket, std.string, std.conv, core.thread, std.stdio, std.datetime;
 
 import webtank.net.http.handler, webtank.net.http.context, webtank.net.http.http;
 
+// Web-сервер порождающий поток на каждое входящее соединение
 class WebServer
 {	
 protected:
@@ -87,22 +88,26 @@ ServerRequest receiveHTTPRequest(Socket sock)
 	
 	//Проверяем размер тела запроса
 	if( contentLength > messageBodyLimit )
+	{
 		throw new HTTPException(
 			"Content length is too large!!!",
 			413 //413 Request Entity Too Large
 		);
+	}
 	
 	string messageBody;
 	char[] bodyBuf;
 	size_t extraBytesInHeaderBuf = startBufLength - headersParser.headerData.length;
 	//Нужно определить сколько ещё нужно прочитать
 	if( contentLength > extraBytesInHeaderBuf )
-	{	bodyBuf.length = contentLength - extraBytesInHeaderBuf;
-		sock.receive(bodyBuf);
-		messageBody = headersParser.bodyData ~ bodyBuf.idup;
+	{
+		bodyBuf.length = contentLength - extraBytesInHeaderBuf + 100;
+		size_t received = sock.receive(bodyBuf);
+		messageBody = headersParser.bodyData ~ bodyBuf[0..received].idup;
 	}
 	else
-	{	messageBody = headersParser.bodyData[0..contentLength];
+	{
+		messageBody = headersParser.bodyData[0..contentLength];
 	}
 	
 	return new ServerRequest( headers, messageBody, sock.remoteAddress, sock.localAddress );
@@ -121,50 +126,155 @@ public:
 		_handler = handler;
 		super(&_work);
 	}
+
+	private void _work()
+	{
+		_processRequest( _socket );
+	}
 	
-protected:
-	void _work()
-	{	ServerRequest request;
-		
-// 		try {
-			request = receiveHTTPRequest(_socket);
-// 		} catch( HTTPException exc ) {
-			//TODO: Построить правильный запрос и отправить на обработку ошибок
-// 			response.clear();
-// 			string statusCodeStr = exc.HTTPStatusCode.to!string;
-// 			string reasonPhrase = HTTPReasonPhrases.get(exc.HTTPStatusCode, "Absolutely unknown status");
-// 			response.headers["status-code"] = statusCodeStr;
-// 			response.headers["reason-phrase"] = reasonPhrase;
-// 			response.write(
-// 				`<html><head><title>` ~ statusCodeStr ~ ` ` ~ reasonPhrase ~ `</title></head><body>`
-// 				~ `<h3>` ~ statusCodeStr ~ ` ` ~ reasonPhrase ~ `</h3>`
-// 				~ `<h4>` ~ exc.msg ~ `</h4>`
-// 				~ `<hr><p style="text-align: right;">webtank.net.web_server</p>`
-// 				~ `</body></html>`
-// 			);
-// 			return;
-// 		}
-		//TODO: Исправить на передачу запроса на страницу
-		//с ошибкой маршрутизатору, а не просто падение сервера
-		if( request is null )
-			return; 
-		
-		auto context = new HTTPContext( request, new ServerResponse(/+&socketSend+/) );
+	mixin ProcessRequestImpl;
+}
+
+import std.parallelism;
+
+ServerResponse makeErrorResponse( Throwable exc )
+{
+	import std.algorithm: castSwitch;
+	import std.conv: text;
+
+	ServerResponse response = new ServerResponse();
+	string statusCode;
+	string reasonPhrase;
+	castSwitch!(
+		(HTTPException e)
+		{
+			statusCode = text( e.HTTPStatusCode );
+			reasonPhrase = HTTPReasonPhrases.get( e.HTTPStatusCode, "Absolutely unknown status" );
+		},
+		(Throwable e)
+		{
+			statusCode = "500";
+			reasonPhrase = HTTPReasonPhrases.get( 500, null );
+		},
+		() {}
+	)(exc);
+
+	response.headers["status-code"] = statusCode;
+	response.headers["reason-phrase"] = reasonPhrase;
+	response.headers["connection"] = "close";
+	response.write(
+		`<html><head><title>` ~ statusCode ~ ` ` ~ reasonPhrase ~ `</title></head><body>`
+		~ `<h3>` ~ statusCode ~ ` ` ~ reasonPhrase ~ `</h3>`
+		~ `<h4>` ~ exc.msg ~ `</h4>`
+		~ `<hr><p style="text-align: right;">webtank.net.web_server</p>`
+		~ `</body></html>`
+	);
+
+	return response;
+}
+
+// Реализация приема и обработки запроса из сокета
+mixin template ProcessRequestImpl()
+{
+	private void _processRequest(Socket sock)
+	{
+		ServerRequest request;
 
 		try {
-		//Запуск обработки HTTP-запроса
-		_handler.processRequest( context );
+			request = receiveHTTPRequest(sock);
 		}
-		catch (Throwable exc) {
+		catch( Throwable exc )
+		{
+			sock.send( makeErrorResponse( exc ).getString() );
+			return;
 		}
-		
-		_socket.send( context.response.getString() ); //Главное - отправка результата клиенту
 
-		
+		if( request is null )
+			return;
+
+		auto context = new HTTPContext( request, new ServerResponse() );
+
+		try {
+			//Запуск обработки HTTP-запроса
+			this._handler.processRequest( context );
+		}
+		catch( Throwable exc )
+		{
+			sock.send( makeErrorResponse( exc ).getString() );
+			return;
+		}
+
+		//Наш сервер не поддерживает соединение
+		context.response.headers["connection"] = "close";
+		sock.send( context.response.getString() ); //Главное - отправка результата клиенту
+
 		scope(exit)
-		{	Thread.sleep( dur!("msecs")( 30 ) );
-			_socket.shutdown(SocketShutdown.BOTH);
-			_socket.close();
+		{
+			sock.shutdown(SocketShutdown.BOTH);
+			Thread.sleep( dur!("msecs")( 30 ) );
+			sock.close();
 		}
 	}
+}
+
+// Web-сервер, использующий стандартный пул задач Phobos
+class WebServer2
+{
+protected:
+	IHTTPHandler _handler;
+	Socket _listener;
+	TaskPool _taskPool;
+	size_t _threadCount;
+	ushort _port = 8082;
+
+public:
+	this(ushort port, IHTTPHandler handler, size_t threadCount = 5)
+	{	_port = port;
+		_handler = handler;
+		_threadCount = threadCount;
+	}
+
+	void _initServer()
+	{
+		_listener = new TcpSocket();
+		assert(_listener.isAlive);
+
+		bool isBinded = false;
+		writeln( "Пытаемся привязать серверный сокет к порту " ~ _port.to!string );
+		while( !isBinded )  //Заставляем ОСь дать нам порт
+		{	try {
+				_listener.bind( new InternetAddress(_port) );
+				isBinded = true;
+
+				//Ждём, чтобы излишне не загружать систему
+				Thread.sleep( dur!("msecs")( 500 ) );
+			} catch(std.socket.SocketOSException) {}
+		}
+		_listener.listen(1);
+		writeln("Сайт стартовал!");
+
+		_taskPool = new TaskPool(_threadCount);
+	}
+
+	private void _runLoop()
+	{
+		while( true )
+		{
+			Socket client = _listener.accept();
+			if( client is null )
+				continue;
+
+			auto newTask = task( &this._processRequest, client );
+			_taskPool.put( newTask );
+		}
+	}
+
+	void start()
+	{
+		_initServer();
+		_runLoop();
+	}
+
+	mixin ProcessRequestImpl;
+
 }
